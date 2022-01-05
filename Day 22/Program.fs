50342bdcc9bee759404efe70df306c661e7218b0
Richard Mills <richardfgmills@gmail.com>
2022-01-05 20:34:21 +0000

    Move to NET 6.0.

--- Day 22/Program.fs
+++ Day 22/Program.fs
@@ -4,6 +4,7 @@ open System.IO
 open System.Text.RegularExpressions
 open FSharpx.Text
 
+
 [<ReferenceEquality>]
 type Node =
     { X: int
@@ -21,9 +22,11 @@ let (|AsInt|_|) (str: string) =
     | true, d -> Some d
     | false, _ -> None
 
+
 [<EntryPoint>]
 let main _ =
 
+    // Stores the nodes in a 2D array.
     let nodes =
         let nodeData =
             File.ReadAllLines "Inputs.txt"
@@ -54,7 +57,7 @@ let main _ =
 
     let nodesFlattened =
         nodes
-        |> Seq.cast<Node>
+        |> Seq.cast
         |> Seq.toArray        
 
     nodesFlattened
@@ -62,14 +65,73 @@ let main _ =
     // Cannot pair up the same nodes.
     |> Seq.filter ((<||) (<>))
     // The first node is not empty and its usage fits within the second's availability.
-    |> Seq.filter (fun ({ Used = used }, { Available = avail }) ->
-        used > 0 && used <= avail)
-    |> Seq.length
+    |> Seq.filter (fun ({ Used = thisUsed }, { Available = otherAvail }) ->
+        thisUsed > 0 && thisUsed <= otherAvail)
+    |> Seq.toArray
+    |> Array.length
     |> printfn "Part 1 answer = %i\n"
 
-    nodesFlattened
-    |> Array.map (fun { X = x; Y = y; Size = size; Used = used; Available = avail } ->
-        sprintf "%i,%i,%i,%i,%i" x y size used avail)
-    |> fun data -> File.WriteAllLines("Nodes.txt", data)
+    let distances =
+        let adjacentOffsets =
+            Array.allPairs [| -1; 0; 1 |] [| -1; 0; 1 |]
+            |> Array.filter (fun (dx, dy) -> Math.Abs (dx + dy) = 1)
+
+        let startingDistances =
+            nodes
+            |> Array2D.map (function
+                | { Used = 0 } -> Some 0
+                | _ -> None)
+
+        let gridWidth, gridHeight =
+            nodes |> Array2D.length1,
+            nodes |> Array2D.length2
+
+        let iteratedDistances =
+            Seq.unfold (fun distances ->
+                let newDistMatrix =
+                    distances
+                    |> Array2D.mapi (fun x y minDist ->
+                        let nodeFrom =
+                            nodes.[x, y]
+
+                        let minAdjacentDist =
+                            adjacentOffsets
+                            |> Array.filter (fun (dx, dy) ->
+                                x+dx >= 0 && x+dx < gridWidth && y+dy >= 0 && y+dy < gridHeight)
+                            |> Array.choose (fun (dx, dy) ->
+                                let nodeTo =
+                                    nodes.[x + dx, y + dy]
+
+                                if nodeTo.Size > nodeFrom.Used then
+                                    distances.[x + dx, y + dy]
+                                else
+                                    None)
+                            |> function
+                                | [||] -> None
+                                | dists -> Some <| Array.min dists
+
+                        match minDist, minAdjacentDist with
+                        | None, None -> None
+                        | None, Some mAD -> Some (mAD + 1)
+                        | Some mD, Some mAD when mAD + 1 < mD -> Some (mAD + 1)
+                        | Some _, _ -> minDist)
+
+                if distances <> newDistMatrix then
+                    Some (newDistMatrix, newDistMatrix)
+                else
+                    None) startingDistances
+
+        iteratedDistances
+        |> Seq.last
+
+    distances
+    |> Array2D.mapi (fun x y -> function
+        | Some d -> $"{x},{y},{d}"
+        | None -> $"{x},{y},X")
+    |> Seq.cast
+    |> Seq.toArray
+    |> fun lines -> File.WriteAllLines ("Distances.txt", lines)  
+    
+    
 
     0
---
+++ Day 23/Program.fs
@@ -0,0 +1,49 @@
+﻿
+open System
+open System.IO
+open System.Text.RegularExpressions
+open FSharpx.Text
+
+
+type ValueOrRegister =
+    | Value of int
+    | Register of string
+
+type Instruction =
+    | Cpy of Source: ValueOrRegister * Register: string
+    | Jnz of Source: ValueOrRegister * Offset: int
+    | Inc of Register: string
+    | Dec of Register: string
+
+
+let (|Match'|_|) =
+    Regex.(|Match|_|) RegexOptions.None
+
+
+let parseInstruction =
+    function
+    | Match' "^cpy (-?\d+) (\w)$" { GroupValues = [ v; tr ] } ->
+        Cpy (Value <| int v, tr)
+    | Match' "^cpy (\w) (\w)$" { GroupValues = [ sr; tr ] } ->
+        Cpy (Register sr, tr)
+    | Match' "^jnz (-?\d+) (-?\d+)$" { GroupValues = [ v; o ] } ->
+        Jnz (Value <| int v, int o)
+    | Match' "^jnz (\w) (-?\d+)$" { GroupValues = [ sr; o ] } ->
+        Jnz (Register sr, int o)
+    | Match' "^inc (\w)$" { GroupValues = [ tr ] } ->
+        Inc tr
+    | Match' "^dec (\w)$" { GroupValues = [ tr ] } ->
+        Dec tr
+    | instr ->
+        failwith $"Unable to parse '{instr}'."
+
+
+[<EntryPoint>]
+let main _ =
+
+    let instructions =
+        File.ReadAllLines "Inputs.txt"
+        |> Array.map (function
+            | Match' @"^cpy "
+
+    0
